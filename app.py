113631da9fd70262793cad31058cfce634d1373c
t3981-h <handoyo.sjarif@gmail.com>
2019-10-11 18:48:06 +0700

    webscrapping branch

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
 from flask import Flask, render_template 
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup 
 
 app = Flask(__name__)
+
+def scrap(url):
+    url_get = requests.get(url)
+    soup = BeautifulSoup(url_get.content,"html.parser")
+    table = soup.find('table', attrs={'class':'table1'})
+    tr = table.find_all('tr')
+
+    temp = [] #initiating a tuple
+
+    for i in range(1, len(tr)):
+        row = table.find_all('tr')[i]
+        
+        #get bulan
+        period = row.find_all('td')[0].text
+        period = period.strip() #for removing the excess whitespace
+        
+        #get inflasi
+        inflation = row.find_all('td')[1].text
+        inflation = inflation.strip() #for removing the excess whitespace
+        
+        temp.append((period,inflation)) 
+    
+    temp = temp[::-1]
+
+    df = pd.DataFrame(temp, columns = ('period','inflation'))
+    df['inflation'] = df['inflation'].str.replace(" %","")
+    df['inflation'] = df['inflation'].astype('float64')
+
+    return df
 
 @app.route("/")
 # This fuction for rendering the table
 def index():
-    df = pd.read_csv("vids.csv", encoding='latin-1') #adding the data frame
+    df = scrap('https://www.bi.go.id/id/moneter/inflasi/data/Default.aspx')
+    df = df.to_html(classes=["table table-bordered table-striped table-dark table-condensed"])
 
-    '''
-    You can prepocess your data here, for example you want to do the group_by or anything else, and put the data frame you want to use at "table".
-    "table" for creating the table, here I only select a couple variable that I want to show. 
-    The output must be to html. Classes are costumization with bootstrap, see bootstrap4 guides for table for more info. 
-    '''
-    table = df[["title","category_id","views","likes","dislikes"]].to_html(classes=["table table-light table-bordered table-striped table-hover .thead-dark"])
-
-    # Example if you want to use sqlite instead of csv
-    # import sqlite3
-    # conn = sqlite3.connect("chinook.db")
-    # albums = pd.read_sql_query("SELECT * FROM albums", conn)
-    # table = albums.to_html(classes=["table table-bordered table-striped table-dark table-condensed"])
-    
-    return render_template("index.html", table=table)
+    return render_template("index.html", table=df)
 
 
 @app.route("/charts")
@@ -29,17 +48,21 @@ def index():
 def charts():
     import altair as alt #using altair library
 
-    df = pd.read_csv("vids.csv", encoding='latin-1') #adding the same dataframe we use
+    df = scrap('https://www.bi.go.id/id/moneter/inflasi/data/Default.aspx')
+    order = df['period'].tolist()
 
     chart = (
         alt.Chart(df)
         .encode( # Put the x and y
-            x="likes", 
-            y="dislikes",
-            color=alt.Color("category_id", scale=alt.Scale(scheme='set2')), #color based on the category, you als can chose the color scheme
-            tooltip=["likes", "dislikes"] 
+            alt.X("period",
+                sort=order
+            ), 
+            alt.Y("inflation",
+                scale=alt.Scale(zero=False)
+            ),
+            tooltip=["period", "inflation"]
         )
-        .mark_circle(size=60) # Choose the plot type here
+        .mark_bar() # Choose the plot type here
         .interactive() # To make the plot interactive 
     )
     return chart.to_json() 
